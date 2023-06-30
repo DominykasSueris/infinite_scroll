1375b382babf760fd1e85de8d03335b83704b1d0
Dominykas Sueris <dominykassueris@gmail.com>
2023-06-30 22:42:04 +0100

    nav element added

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -7,16 +7,21 @@ const MainPage = () => {
   if (!data) return <h1>No data</h1>;
 
   return (
-    <div className="container">
-      {data.photos.photo.map((p) => (
-        <img
-          className="col-4"
-          key={p.id}
-          src={`https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}.jpg`}
-          alt={p.title}
-        />
-      ))}
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top h-3 d-flex justify-content-end">
+        <span className="navbar-brand">Lorem Ipsum</span>
+      </nav>
+      <div className="container">
+        {data.photos.photo.map((p) => (
+          <img
+            className="col-4"
+            key={p.id}
+            src={`https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}.jpg`}
+            alt={p.title}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
